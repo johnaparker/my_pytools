9849bf5750023a3a10b74acfb6fa1553862e7a7a
John <japarker@uchicago.edu>
2017-01-08 17:49:49 -0600

    added custom scientific axis

--- my_pytools/my_matplotlib/plots.py
+++ my_pytools/my_matplotlib/plots.py
@@ -3,17 +3,25 @@ import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib import ticker
+from matplotlib.ticker import MaxNLocator,MultipleLocator, FormatStrFormatter, FuncFormatter, ScalarFormatter
 
-def mkcmap(): 
+
+def phase_cmap(): 
+    """ Return a black-white scale phase color map"""
     white = '#ffffff'
     black = '#000000'
     red = '#ff0000'
     blue = '#0000ff'
     anglemap = matplotlib.colors.LinearSegmentedColormap.from_list(
-        'anglemap', [black, red, white, blue, black], N=256, gamma=1)
+        'anglemap', [black, white, black], N=256, gamma=1)
     return anglemap
 
 def pcolor_z_info(data,xdata,ydata, ax=None):
+    """ Allow pcolor data to be seen interactively in the plot
+            data        2-d data
+            xdata       1-d x data
+            ydata       1-d y data
+            ax          axis            """
     if not ax:
         ax = plt.gca()
     numrows, numcols = data.shape
@@ -115,4 +123,44 @@ def top_colorbar(size="3%", pad=0.15, label=None):
     cb.locator = tick_locator
     cb.update_ticks()
     return cb
+
+
+def scientific_axis(precision=1, ax=None):
+    """create a scientific_axis on the y-axis
+            precision       floating point precision
+            ax              axis to be used """
+    if not ax:
+        ax = plt.gca()
+    ymin, ymax = ax.get_ylim()
+    # x = "%.{}e" % ymax 
+    x = "{0:.{1}e}".format(ymax,precision)
+
+    pos = x.find('+')
+    sgn = ''
+    if pos == -1:
+        pos = x.find('-')
+        sgn = '-'
+    n = int(x[pos+1:])
+    if sgn == '-':
+        n *= -1
+
+    def formatter(xtick , pos):
+        return '{0:.{1}f}'.format(xtick/10**n,precision)
+
+    bbox = ax.get_position()
+    x,y = bbox.corners()[1]
+    buff = bbox.height*0.01
+    plt.figtext(x,y+buff, r'$\times \mathregular{{10^{{ {0} }} }}$'.format(n))
+    # embed()
+
+    ax.yaxis.set_major_formatter( FuncFormatter(formatter) )
+    # plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    # plt.gca().yaxis.set_major_formatter( FormatStrFormatter('%.1f') )
+
+def set_num_ticks(min_ticks, max_ticks, axis='both'):
+    """ Set bounds on the number of ticks """
+    if axis == 'both' or axis == 'x':
+        plt.gca().xaxis.set_major_locator(MaxNLocator(min_n_ticks=min_ticks, nbins=max_ticks))
+    if axis == 'both' or axis == 'y':
+        plt.gca().yaxis.set_major_locator(MaxNLocator(min_n_ticks=min_ticks, nbins=max_ticks))
 
