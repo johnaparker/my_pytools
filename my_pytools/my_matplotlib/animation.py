5a5d7295e1a6b94d55d30eb76d1faff61cdffc33
John <japarker@uchicago.edu>
2017-08-09 02:20:38 -0500

    new animations module; currently empty

---
+++ my_pytools/my_matplotlib/animation.py
@@ -0,0 +1,14 @@
+"""Helper functions to perform animations"""
+
+# Given X[N], Y[N,T], plot animated line
+# Given f(x), f(y,t), plot animated line
+# Repeat for X[Nx,Ny], etc, using pcolormesh or imshow and functional versions
+    # included cmap/other controls for signed data vs unsigned
+# For X,Y,U,V data, allow animated quiver
+
+# animated sprites script. Allow  choice of filled vs. unfilled, full color control.
+    # include option to show a trail of the trajectories
+    # include rotations (circles included)
+
+# possibly allow sprites, pcolormesh, and quiver animations to be overlapped easily
+
