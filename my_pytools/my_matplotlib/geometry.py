76080e9d46c5aa50e039d2aebf3b87593a41ab0d
John <japarker@uchicago.edu>
2017-08-09 02:13:41 -0500

    new geometry module

---
+++ my_pytools/my_matplotlib/geometry.py
@@ -0,0 +1,11 @@
+"""Helper functions for 2d sprites"""
+
+def rotation_transform(axis, angle, ax = None):
+    """Return a rotation transfrom that rotates around an axis by an angle
+            axix[2]       (x,y) center of rotation
+            angle         angle of rotation (in degrees)  """
+
+    if ax is None: ax = plt.gca()
+    t_scale = ax.transData
+    t_rotate = mpl.transforms.Affine2D().rotate_deg_around(axis[0], axis[1], angle*180/np.pi)
+    return t_rotate + t_scale
