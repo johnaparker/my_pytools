f3a101af6fa4fc7086244289fc2f37b44c885045
John <japarker@uchicago.edu>
2017-08-16 00:40:42 -0500

    h5cache added

--- my_pytools/my_h5py.py
+++ my_pytools/my_h5py.py
@@ -10,3 +10,33 @@ def write_over(group, name, data):
         group[name][...] = data
     else:
         group[name] = data
+
+class h5cache:
+    def __init__(self, shape, size, dtype):
+        """cache a numpy array
+                shape       size of numpy array
+                size        number of records to be cached
+                dtype       array datatype
+        """
+        cache_shape = (size,) + shape
+
+        self.size = size
+        self.current_record = 0
+        self.shape = shape
+        self.cache = np.zeros(cache_shape, dtype=dtype)
+
+    def add(self, record):
+        """add a record to the cache"""
+        # require shape(record) == self.shape
+        self.cache[self.current_record] = record
+        self.current_record += 1
+
+    def is_full(self):
+        """return true if the cache is full"""
+        return self.current_record == self.size
+
+    def clear(self):
+        """empty the cache (cached data will be overwritten in future adds)"""
+        self.current_record = 0
+
+
