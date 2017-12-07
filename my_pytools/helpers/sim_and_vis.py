982d465fc1b28fc1ebd6fd1e887b60b39b3404cb
John <japarker@uchicago.edu>
2017-12-06 18:02:19 -0600

    new version of sim and vis

--- my_pytools/helpers/sim_and_vis.py
+++ my_pytools/helpers/sim_and_vis.py
@@ -43,8 +43,12 @@ class sim_and_vis:
 
     def __init__(self, filepath):
         """filepath to hdf5 file"""
-        self.filepath = filepath
 
+        self.filepath = filepath
+        self.simulation_functions = []
+        self.visualization_functions = []
+
+    def run_parser(self):
         parser = argparse.ArgumentParser()
         parser.add_argument('action', nargs='?', type=str, choices=['sim', 'vis', 'both'], default='vis', help='Run the sim, vis the results, or do both')
         parser.add_argument('-f', '--force', action='store_true', help='force over-write any existing files')
@@ -72,14 +76,14 @@ class sim_and_vis:
         """read symbols"""
         return load_symbols(self.filepath)
 
-    def execute(self, sims, vis, store=None):
-        """given {name: sim()} dictionary and {name: vis()} dictionary, run the request
+    def execute(self, store=None):
+        """Run the requested simulations and visualizations
            
            Arguments:
-               sims        {name: sim()} dictionary of simulation names to simulation functions
-               vis         {name: vis()} dictionary of visualization names to visualization functions
                store       {name: data} dictionary to write as additional data (optional)
         """
+
+        self.run_parser()
 
         if store is not None:
             with h5py.File(self.filepath, 'a') as f:
@@ -90,12 +94,14 @@ class sim_and_vis:
 
         with h5py.File(self.filepath, 'a') as f:
 
-            for name,sim in sims.items():
+            for sim in self.simulation_functions:
+                name = sim.__name__
                 if self.args.action in ['sim', 'both'] or name not in f:
                     if not (self.args.sims is not None and name not in self.args.sims):
                         self.request(name)
 
-            for name,sim in sims.items():
+            for sim in self.simulation_functions:
+                name = sim.__name__
                 if self.args.action in ['sim', 'both'] or name not in f:
                     if self.args.sims is not None and name not in self.args.sims:
                         continue
@@ -108,9 +114,25 @@ class sim_and_vis:
 
         if self.args.action in ['vis', 'both']:
             print("Visualizing data...")
-            for name,func in vis.items():
+            for func in self.visualization_functions:
                 func()
 
+    def simulation(self, description=None):
+        def wrap(func):
+            self.simulation_functions.append(func)
+            print(f'{func.__name__}: {description}')
+            return func
+        return wrap
+
+    def visualization(self, description=None):
+        def wrap(func):
+            self.visualization_functions.append(func)
+            print(f'{func.__name__}: {description}')
+            return func
+        return wrap
+
+    def shared(self, class_type):
+        return class_type
 
 
 if __name__ == "__main__":
@@ -124,16 +146,29 @@ if __name__ == "__main__":
     ### Fast, shared code goes here
     x = np.linspace(0,1,10)
 
+    @job.shared
+    class variables():
+        def __init__(self):
+            self.x = np.linspace(0,1,10)
+
+        def writer(self):
+            pass
+
     ### Slow, sim-only code goes here and relevant data is written to file
+    @job.simulation("compute the square of x")
     def sim1():
+        # shared = job.get_shared()
+        # shared.x
+
         y = x**2
-        return {'y': y**2}
+        return {'y': y}
 
+    @job.simulation("compute the cube of x")
     def sim2():
-        z = x**2
-        return {'z': z**3}
+        z = x**3
+        return {'z': z}
 
-    ### Process/Visualize data here after loading symbols from file
+    @job.visualization("visualize the data")
     def vis():
         sim = job.read()
         plt.plot(x,sim.sim1.y)
@@ -141,4 +176,4 @@ if __name__ == "__main__":
         plt.show()
 
     ### execute
-    job.execute({'sim1': sim1, 'sim2': sim2}, {'vis': vis}, store={'x': x})
+    job.execute(store={'x': x})
